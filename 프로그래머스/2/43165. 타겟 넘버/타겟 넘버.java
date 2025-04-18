e0d4f276e65a8a2f7397c90f07811dbdd695c2ef
Juyeon Nam <jynam6069@gmail.com>
2025-04-18 20:01:18 +0900

    [level 2] Title: 타겟 넘버, Time: 9.82 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,49 +1,19 @@
-import java.util.*;
-
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        int calculatedNum = 0;
-        //dfs(0, 0, target, numbers);
-        
-        int n = numbers.length;
-        bfs(numbers, target);
+        dfs(0, target, numbers, 0);
         return answer;
     }
     
-    public void bfs(int[] numbers, int target) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentSum = current[0];
-            int idx = current[1];
-
-            if (idx == numbers.length) {
-                if (currentSum == target) {
-                    answer++;
-                }
-                continue;
-            }
-
-            queue.offer(new int[]{currentSum + numbers[idx], idx + 1});
-            queue.offer(new int[]{currentSum - numbers[idx], idx + 1});
-        }
-    }
-    
-/*
-    public void dfs(int calculatedNum, int index, int target, int[] arr){
-        // Base Case
-        if(calculatedNum == target && arr.length == index+1){
+    public void dfs(int depth, int target, int[] numbers, int result){
+        if(numbers.length == depth  && result == target){
             answer++;
             return;
         }
-        if(arr.length <= index+1){
+        if(numbers.length == depth){
             return;
         }
-        dfs(calculatedNum+arr[index+1], index+1, target, arr);
-        dfs(calculatedNum-arr[index+1], index+1, target, arr);
-    }*/
-    
+        dfs(depth+1, target, numbers, result-numbers[depth]);
+        dfs(depth+1, target, numbers, result+numbers[depth]);
+    }
 }
