dba51a7c75050f2b5478f58d3a045023b29f0782
Juyeon Nam <jynam6069@gmail.com>
2025-05-11 01:12:47 +0900

    [level 3] Title: 단어 변환, Time: 0.94 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,3 +1,4 @@
+/*
 import java.util.*;
 
 class Solution {
@@ -39,6 +40,75 @@ class Solution {
         int step;
 
         Word(String word, int step) {
+            this.word = word;
+            this.step = step;
+        }
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        boolean[] visited = new boolean[words.length];
+        Queue<Words> queue  = new LinkedList<>();
+        queue.offer(new Words(begin,0));
+        while(!queue.isEmpty()){
+            Words current = queue.poll();
+            String cw = current.word;
+            int cs = current.step;
+            if(cw.equals(target)){
+                return cs;
+            }
+            for(int i=0; i<words.length; i++){
+                if(!visited[i] && canConvert(cw, words[i])){
+                    visited[i] = true;
+                    queue.offer(new Words(words[i],cs+1));
+                }
+            }
+        }
+        return 0;
+        
+    }
+    public boolean canConvert(String word1, String word2){
+        int count = 0;
+        for(int i=0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i) ){
+                count++;
+            }
+        }
+        if(count == 1){
+            return true;
+        }
+        return false;
+    }
+    
+    class Words{
+        String word;
+        int step;
+        
+        Words(String word, int step){
             this.word = word;
             this.step = step;
         }
